0f618c301687557b411a084e3e5131552e772e57
sadafmirza98 <sadafmirza98@gmail.com>
2024-12-29 19:02:02 +0530

    login fix

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -16,7 +16,6 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   onLogin() {
-    console.log('Login button clicked'); // Log button click
     if (!this.email || !this.password) {
       this.errorMessage = 'Please fill in both fields';
       return;
@@ -27,12 +26,10 @@ export class LoginComponent {
 
     this.authService.login(this.email, this.password).subscribe(
       (response) => {
-        console.log('jyfjfj', response); // Log successful login
         this.isLoading = false; // Hide loader
         this.router.navigate(['/goals']); // Redirect to the main page
       },
       (error) => {
-        console.log('Login error', error); // Log login error
         this.isLoading = false; // Hide loader
         this.errorMessage = error; // Display error message
       }
